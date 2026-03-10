899b4bfe313b741a4145777c01701743ab799132
Avanti Joshi <joshiavanti2004@gmail.com>
2026-03-10 19:22:24 +0530

    Update Report.js

--- server/models/Report.js
+++ server/models/Report.js
@@ -4,22 +4,22 @@ const reportSchema = new mongoose.Schema({
   title: String,
   date: String,
   duration: String,
-  agenda: String,
+  objective: String,
   summary: String,
   classType: [String],
+  whocanattend: [String], 
   organizedBy: String,
   speakerName: String,
   speakerDesignation: String,
-
   sessionRoles: {
     hod: String,
     coordinator: String,
     anchor: String,
     voteOfThanks: String,
   },
-
-  noticeFile: [String],
-  photos: [String],
+  noticeFile: [String], 
+  photos: [String], 
+  registrationLink: String,
   createdBy: String
 }, { timestamps: true });
 
